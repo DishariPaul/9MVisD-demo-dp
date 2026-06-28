e51c9d3ef1b1ef160c9788b623baeeb08ec45151
Dishari Paul <dishari.myworld@gmail.com>
2026-06-28 13:01:30 +0530

    Configure backend to use Supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "9MVisD",
-    password: "gre_Dishi_post29",
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
+
